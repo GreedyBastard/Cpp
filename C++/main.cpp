cf4e010e1e4b217bc7ba7f5de6eb2d5e06eefd72
Victor Popescu <nomevictor@gmail.com>
2015-11-15 19:08:17 +0200

    another commit

--- C++/main.cpp
+++ C++/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a;
+    int b;
+    int sum;
+
+    cout << "This is a very simple example of a sum!" << endl;
+
+    cout << "Enter a number! \n";
+    cin >> a;
+
+    cout << "Enter another number! \n";
+    cin >> b;
+
+    sum = a + b;
+    cout << "The sum of those numbers is: " << sum << endl;
+
     return 0;
 }
