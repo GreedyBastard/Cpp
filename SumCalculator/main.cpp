78526d489031b5728b538dd4a58e77e80aad14c6
Victor Popescu <nomevictor@gmail.com>
2015-11-15 19:43:50 +0200

    Rename
    Correcting file names

--- SumCalculator/main.cpp
+++ SumCalculator/main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main()
 {
+    //Declaring variables
     int a;
     int b;
     int sum;
 
+    //Store data inside variables
     cout << "This is a very simple example of a sum!" << endl;
 
     cout << "Enter a number! \n";
@@ -16,8 +18,9 @@ int main()
     cout << "Enter another number! \n";
     cin >> b;
 
+    //Calculate the sum and print the answer
     sum = a + b;
-    cout << "The sum of those numbers is: " << sum << endl;
+    cout << "The sum of those numbers is: " << sum << " potatoes, and I'm sure of that!" << endl;
 
     return 0;
 }
