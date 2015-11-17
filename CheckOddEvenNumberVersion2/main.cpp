b4c34175d1d4c86f82c4161ed5d1a0a23e3e8396
Victor Popescu <nomevictor@gmail.com>
2015-11-17 16:02:26 +0200

    version2

--- CheckOddEvenNumberVersion2/main.cpp
+++ CheckOddEvenNumberVersion2/main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
+/* VERSION 2.
+*  Checking if a number is odd or even,
+*  using ternary operators! Much more simpler!
+*
+* To check whether an integer is even or odd,
+* the remainder is calculated when it is divided
+* by 2 using modulus operator %. If remainder is zero,
+* that integer is even, if not, that integer is odd.
+*/
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n;
+    cout << "This program check if a number is odd or even!" << endl;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    //Checking:     //If it is == 0 then:       //If it's not:
+    ( n % 2 == 0) ? cout << n << " is even!" : cout << n << " is odd!";
+
     return 0;
 }
