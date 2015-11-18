b816a6472bfe4b9a9f42905311012f93b0237229
Victor Popescu <nomevictor@gmail.com>
2015-11-18 21:30:26 +0200

    Version 2 advanced

---
+++ StockMarketSimulatorAdvanced/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*  VERSION 2.
+*
+*  Stock market simulator. Advanced version using objects and functions.
+*  For simple explanations see the VERSION 1 file.
+*
+*  It calculates how much money you will earn if you
+*  quit programming and start doing stock market trades :D
+*
+*/
+
+int main()
+{
+
+}
