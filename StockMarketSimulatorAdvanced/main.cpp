04396e33c693611286d9ae517f4d5465ace55c13
Victor Popescu <nomevictor@gmail.com>
2015-11-18 22:26:51 +0200

    Version 2

--- StockMarketSimulatorAdvanced/main.cpp
+++ StockMarketSimulatorAdvanced/main.cpp
@@ -13,7 +13,52 @@ using namespace std;
 *
 */
 
+class keepFunctions{
+
+public:
+
+    void firstMessage(){
+
+        cout << " Lets make some money! \n" << endl;
+    }
+    float doTheMath(float a, float r, int days){
+
+        for(int x = 1; x <= days ; x++){
+
+            total = a * pow(1+r/100, x);
+            cout <<" Day " << x << " ----- " << total << " $$$" << endl;
+        }
+
+    }
+
+private:
+
+    float total;
+
+
+};
+
 int main()
 {
+    float a;
+    float r;
+    int days;
+
+    keepFunctions obj;
+    obj.firstMessage(); //print the first message
+
+    cout << " Enter how much cash you wanna invest: " ;
+    cin >> a;
+    cout << " Enter interest rate as % : " ;
+    cin >> r;
+    cout << " How may days you want to keep the investment: " ;
+    cin >> days;
+
+    obj.doTheMath(a,r,days);
+
+
+    //ok we got the user inputs and stored them in some variables
+
+
 
 }
