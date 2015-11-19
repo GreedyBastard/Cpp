fe7c048636b24b4aca78cd33c3876a9323383e1e
Victor Popescu <nomevictor@gmail.com>
2015-11-19 05:14:20 +0200

    Switch Statement

--- SwitchStatement/main.cpp
+++ SwitchStatement/main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+/*
+* Basic example of a switch statement
+*/
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int age = 21;
+
+    switch(age){
+        case 16: //for age of 16:
+            cout << " Too young!" << endl;
+            break; //break will end the switch when it gets a match
+        case 18: //for age of 18:
+            cout <<" Maybe you can drive!" << endl;
+            break;
+        case 21: //for age of 21:
+            cout << " Ok, now we talk!" << endl;
+            break;
+        default: //for any other age:
+            cout << " You don't belong here..." << endl;
+
+
+
+    }
 }
