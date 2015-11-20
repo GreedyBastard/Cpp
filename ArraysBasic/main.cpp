e54985cd8ed56bb42b433fdad82c038cf83bf9f2
Victor Popescu <nomevictor@gmail.com>
2015-11-20 16:48:31 +0200

    Basic Array

---
+++ ArraysBasic/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+*  Basic array example
+*/
+
+int main()
+{
+        //      0   1   2  3  4   // array index
+    int a[5] = {23,-23, 4, 5, 6}; // 5  elements array
+    cout << a[2] << endl;
+}
