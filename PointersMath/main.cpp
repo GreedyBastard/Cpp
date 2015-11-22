ff9e96ec15a0e46e1a0aba270251622896faab0c
Victor Popescu <nomevictor@gmail.com>
2015-11-22 14:50:01 +0200

    What pointer + int does

---
+++ PointersMath/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+/*
+* Each int element is stored 4 bytes away from the previous one
+* because 1 int take 4 bytes of space to be stored
+*/
+
+int main()
+{
+    int test[5]; //array
+    int *p0 = &test[0]; //point to the address of the first array element
+    int *p1 = &test[1]; //--//-- second
+    int *p2 = &test[2]; //--//-- third
+
+
+    cout << "p0 address: " << p0 << endl;
+    cout << "p1 address: " << p1 << endl;
+    cout << "p2 address: " << p2 << endl;
+
+    p0 += 2; //add 2 to pointer, it changes what element will point to
+              //in this example, adding 2, will point to the element stored 2 indexes away
+              //and that element is bp2
+
+
+    cout << "p0+2 address: " << p0 << endl;
+
+}
