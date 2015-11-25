e9b357d57ae20f6485b631ba8ca3ebb8f11bccfd
Victor Popescu <nomevictor@gmail.com>
2015-11-25 18:55:40 +0200

    Operators Overloading Ex2

--- OperatorsOverloadingV2/main.cpp
+++ OperatorsOverloadingV2/main.cpp
@@ -3,15 +3,23 @@
 
 using namespace std;
 
+/*
+* Using "+" instead of a function name
+*/
+
 int main()
 {
 
-    secondClass a(34);//take 34
-    secondClass b(21);//take 21
-    secondClass c; //blank object
+    secondClass a(34);//num 34
+    secondClass b(21);//num 21
+    secondClass c; //create  a blank object
 
-    //takes a value (34) and add b value (21) and set the value of C
-    c = a + b; //+
+    //takes a value (34) and add b value (21) and set the value of c
+    c = a + b; //take the current object (a) and trigger b as a parameter
+               //and return a brandNew secondClass object with 55 as its number
+               //as its return value we set it = to c
+               //so c = brandNew secondClass object with num = 55
+
     cout << c.num <<endl;
 
 }
