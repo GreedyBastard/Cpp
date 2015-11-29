7e2404085a6aa71a7d1259f13c37e50fc402142e
Victor Popescu <nomevictor@gmail.com>
2015-11-29 17:53:15 +0200

    Reading from a file

---
+++ FilesV3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+/*
+* Read info from a file
+*/
+
+int main()
+{
+    //
+    ifstream theFile("players.txt");
+
+    int id;
+    string name;
+    double money;
+
+    while(theFile >> id >> name >> money){ //read every line until the file pointer is at the end of the file
+        cout << id << ", " << name << ", " << money << endl;
+    }
+    //we don't have to close the file with object.close(), because when the "while" loop return false, the destructor will delete the object
+
+}
